406bac87226ee85b7448867e9ef22222d3ee8cb7
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-26 21:58:43 -0300

    Fetching openweather API

--- website/app.js
+++ website/app.js
@@ -1,5 +1,27 @@
 /* Global Variables */
+const openWeatherApiKey = 'f1ee58157d6ebd35f1924b75f633d249';
+const generateButton = document.getElementById('generate');
+const zipInput = document.getElementById('zip');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+const getWeather = async () => {
+    const zip = zipInput.value;
+    if (zip.length != 5) {
+        console.log('Bad zip');
+        return;
+    }
+    const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${openWeatherApiKey}`;
+    const res = await fetch(url);
+    try {
+        const data = await res.json();
+        console.log(data);
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+// Event listeners
+generateButton.addEventListener('click', getWeather);
