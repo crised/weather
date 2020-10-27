600fc1f54b325ef37f0b6c5233ee033187131c38
Cristian Edwards <cristian.edwards@bairesdev.com>
2020-10-26 21:30:45 -0300

    Server serving static content

--- server.js
+++ server.js
@@ -2,18 +2,35 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
+const port = 3000;
+
+const server = app.listen(port, listening);
+function listening(){
+    console.log(server);
+    console.log(`running on localhost: ${port}`);
+};
+
 
 
 // Setup Server
+const openWeatherApiKey = 'f1ee58157d6ebd35f1924b75f633d249';
+const cityName = 'Miami';
+const url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openWeatherApiKey}`;
